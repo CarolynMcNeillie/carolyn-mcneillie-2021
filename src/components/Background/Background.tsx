42f6f3b924ca91da7ef6a5121950c83385209a3d
CarolynMcNeillie <carolynmcneillie@gmail.com>
2023-03-11 16:58:24 -0800

    start to set up foundations

---
+++ src/components/Background/Background.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Background = () => {
+  return (
+    <div className="background">
+        <div>
+            <div className="cell light"></div>
+            <div className="cell dark"></div>
+            <div className="cell medium"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Background
