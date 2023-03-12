550ea1a55262b8baed534f298a361550b3f9e7c6
CarolynMcNeillie <carolynmcneillie@gmail.com>
2023-03-12 19:25:07 -0400

    wip

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -2,11 +2,24 @@ import React, {useMemo} from "react"
 import { motion } from "framer-motion";
 
 import tiles from './tiles'
+import themes from './themes'
 
 const cells = ['dark', 'medium', 'light']
 
+const currentTile = 0
+
+function updateTheme(index: number) {
+  const newTheme = themes[index]
+  Object.keys(newTheme).forEach(key => {
+    document.documentElement.style.setProperty(key, newTheme[key]);
+  })
+}
+
+updateTheme(1)
+
+
 const generateTile = () => {
-  const size = tiles[0].size * 10
+  const size = tiles[currentTile].size * 10
   return (
     <svg 
       xmlnsXlink="http://www.w3.org/1999/xlink" 
@@ -14,9 +27,9 @@ const generateTile = () => {
       viewBox={`0 0 ${size} ${size}`}
       className="tile"
       >
-      {tiles[0].pattern.map((color, index) => {
-        const xPosition = index % tiles[0].size
-        const yPosition = Math.floor(index / tiles[0].size)
+      {tiles[currentTile].pattern.map((color, index) => {
+        const xPosition = index % tiles[currentTile].size
+        const yPosition = Math.floor(index / tiles[currentTile].size)
       return color > 0 ? (
         <motion.rect
           x={xPosition * 10}
---
+++ src/components/Background/themes.ts
@@ -0,0 +1,24 @@
+const themes: any = [
+    {
+        '--light': '#e6cbc9',
+        '--medium': '#344767',
+        '--dark': '#242236',
+    }, 
+    {
+        '--light': '#2e2f2e',
+        '--medium': '#1d2e1d',
+        '--dark': '#f6cbc9',
+    },
+    {
+        '--light': '#e6cbc9',
+        '--medium': '#344767',
+        '--dark': '#242236',
+    },
+    {
+        '--light': '#e6cbc9',
+        '--medium': '#344767',
+        '--dark': '#242236',
+    },
+]
+
+export default themes
