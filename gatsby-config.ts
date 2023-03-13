7add2d82bee4956416bed4f44530f171ce9722f1
CarolynMcNeillie <carolynmcneillie@gmail.com>
2023-03-12 22:35:09 -0400

    wip

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,6 @@ const config: GatsbyConfig = {
     title: `CarolynMcNeillie`,
     description:
     "Carolyn McNeillie is a Senior Font-End Developer.",
-    image: "images/Carolyn.png",
     twitterUsername: "@carolynalive",
     siteUrl: "https://carolynmcneillie.com",
   },
@@ -13,36 +12,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "UA-113885017-1",
-      },
-    },
-    "gatsby-plugin-fontawesome-css",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-google-gtag",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [`montserrat:400,700`, `cardo`],
-        display: "swap",
-      },
-    },
-  ]
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap"]
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,54 @@ import type { HeadFC, PageProps } from "gatsby"
 import "../styles/normalize.css"
 import "../styles/styles.scss"
 import Background from "../components/Background/Background"
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.4,
+      delay: 2,
+      delayChildren: 2.8,
+      staggerChildren: 0.2
+    }
+  }
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
       <Background />
-      {/* <h1>Hello</h1> */}
+      <motion.div 
+        className="card"
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
+          <motion.h1 variants={item}>Carolyn McNeillie</motion.h1>
+          <motion.h2 variants={item}>
+            Toronto / Hamilton / Anywhere <br />
+            <a href="mailto:hello@carolynmcneillie.com">
+              hello@carolynmcneillie.com
+            </a>
+          </motion.h2>
+          <motion.p className="text" variants={item}>
+            I am a Senior Web Developer with a background in design, marketing, e-commerce, book publishing, mural painting, and{" "}
+            art restoration. Using React, TypeScript,
+            and GraphQL, I have helped build e-commerce, websites, and web
+            applications for clients including House of Anansi, Green Shield, & Shopify. I ♥️ CSS.
+          </motion.p>
+      </motion.div>
     </main>
   ) 
 }
