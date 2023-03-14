f638fb871249ccd081abd86ac07aba3eb5f2b910
CarolynMcNeillie <carolynmcneillie@gmail.com>
2023-03-13 21:32:57 -0400

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -124,13 +124,13 @@ export const Head: HeadFC = () => (
     <html lang="en" />
     <title>Carolyn McNeillie</title>
     <meta name="description" content="I am a senior front-end developer who has been crafting exceptional web experiences for five years. I love CSS." />
-    <link id="icon" rel="icon" href="icon-specific-for-this-page" />
-    <meta name="image" content="./images/Carolyn.png" />
+    <link id="icon" rel="icon" href="../images/icon.png" />
+    <meta name="image" content="../images/Carolyn.png" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="Carolyn McNeillie" />
     <meta name="twitter:url" content="https://www.carolynmcneillie.com"/>
     <meta name="twitter:description" content="I am a senior front-end developer who has been crafting exceptional web experiences for five years. I love CSS." />
-    <meta name="twitter:image" content="./images/Carolyn.png" />
+    <meta name="twitter:image" content="../images/Carolyn.png" />
     <meta name="twitter:creator" content="carolynalive" />
   </>
 )
